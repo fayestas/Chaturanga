df42ad71a742866d0a3f7ac29c2dbdafd5302cdf
fayestas <ayestasfrances@unitec.edu>
2016-08-18 11:59:19 -0600

    primera parte
    by frances

---
+++ src/chaturanga/Chaturanga.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chaturanga;
+
+/**
+ *
+ * @author Frances
+ */
+public class Chaturanga {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/chaturanga/Ficha.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chaturanga;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Frances
+ */
+public abstract class Ficha {
+    
+    String color;
+    ArrayList<String> capturas = new ArrayList<>();
+    
+    
+    
+    
+}
