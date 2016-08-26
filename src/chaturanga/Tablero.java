cd2660785840a81a4bec611347422f299e1b7904
María Celeste Cabrera <Maryblue@unitec.edu>
2016-08-26 15:38:36 -0600

    Posicionamos las fichas en el tablero

--- src/chaturanga/Tablero.java
+++ src/chaturanga/Tablero.java
@@ -13,4 +13,43 @@ public class Tablero {
     
     Ficha [][] tablero = new Ficha[8][8];
     
+    public boolean setFichas(){
+        
+    }
+    
+    public void setPeones(){
+        int fila = 1;
+        for(int c=0; c<8; c++){
+            tablero[fila][c] = new Peon();
+            if(c==7 && fila==1){
+                c=0;
+                fila=6;
+            }
+        }
+    }
+    
+    public void setOtras(){
+        setAddOtras();
+        tablero[7][3]= new Consejero();
+        tablero[0][4]= new Consejero();
+        tablero[0][3]= new Rey();
+        tablero[7][4]= new Rey();
+                
+    }
+    
+    public void setAddOtras(){
+        int f=0;
+        
+        do{
+            tablero[f][0] = new Torre();
+            tablero[f][1] = new Caballo();
+            tablero[f][2] = new Elefante();
+            tablero[f][5] = new Elefante();
+            tablero[f][6] = new Caballo();
+            tablero[f][7] = new Torre();
+            f=7;
+        }while(f!=7);
+    }
+    
 }
+
